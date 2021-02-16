ec9964e291466aa893b36e04c71fce0f4bb80b5f
ori <ori101914@gmail.com>
2021-02-16 10:19:08 +0200

    main

---
+++ main.cpp
@@ -0,0 +1,43 @@
+ /** 
+ *   Author: Ori Azulay
+ */
+
+#include <iostream>
+#include <stdio.h>
+#include <string.h>
+#include "dummyClasses.cpp"
+#include "forConst.cpp"
+#include "Polymo.cpp"
+#include "temptest.cpp"
+#include "Decorator.cpp"
+#include "bridge.cpp"
+#include "compositeDP.cpp"
+#include "factoryDP.cpp"
+
+using namespace std;
+
+int main(){
+// meal* tommy = new tomato(new meat(new bread()));
+// tommy->add();
+
+//    HouseType* a = new BigHouse(new StrawHouse());
+//    a->BuildHouse();
+//    HouseType* b = new RoundHouse(new WoodHouse());
+//    b->BuildHouse();
+
+//    Compenent *c = new Composite("c");
+//    Compenent *c1 = new Composite("c1");
+//    Compenent *c2 = new Composite("c2");
+//    Compenent *l = new Leaf("l");
+//    Compenent *l1 = new Leaf("l1");
+//    Compenent *l2 = new Leaf("l2");
+//    c->Add(c1);
+//    c->Add(c2);
+//    c->Add(l);
+//    c2->Add(l2);
+//    c1->Add(l1);
+//    c->GetChild();
+
+
+   return 0;
+}
